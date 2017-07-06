03e65a4dd749bee2e474084d001a6dd36886a849
Yury Gaydaychuk <agri1987cola@gmail.com>
2017-07-06 16:23:56 +0300

    newnew

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+//Перенаправление при нажатии кнопки. Оба view (до нажатия и после нажатия) представлены в виде отдельных html файлов. Шаблонизация не применяется. Использованы express, http, fs
 const express = require('express')
+var http = require('http');
+var fs = require('fs');
+
 const app = express()
 app.use(express.static('input'))
 app.use(express.static('output'))
 
 app.get('/', function (req, res) {
-    res.redirect('input/input.html');
+    fs.readFile('input/input.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 })
 
 app.post('/form_handler', function (req, res) {
-    res.redirect('output/output.html');
+    fs.readFile('output/output.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 })
 
 app.listen(3000, function () {
